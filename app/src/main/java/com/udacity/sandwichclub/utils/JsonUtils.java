973782b01b193454d7869021fdf44f097462473c
Oscar Reyes <oscarr.reyes17@gmail.com>
2020-01-20 22:34:46 -0500

    + sandwitch parsing in JsonUtils
    * Added code functionality to parse a JSON string of sandwich into a Sandwich instance

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,91 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
-public class JsonUtils {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
-    }
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class JsonUtils {
+	private static final String TAG = JsonUtils.class.getSimpleName();
+
+
+	public static Sandwich parseSandwichJson(String json) {
+		Sandwich sandwich = new Sandwich();
+
+		try {
+			JSONObject sandwichObject = new JSONObject(json);
+
+			parseSandwichName(sandwich, sandwichObject.getJSONObject("name"));
+
+			if (sandwichObject.has("placeOfOrigin"))
+				sandwich.setPlaceOfOrigin(sandwichObject.getString("placeOfOrigin"));
+			if (sandwichObject.has("description"))
+				sandwich.setDescription(sandwichObject.getString("description"));
+			if (sandwichObject.has("image")) sandwich.setImage(sandwichObject.getString("image"));
+
+			if (sandwichObject.has("ingredients")) {
+				final JSONArray jsonArray = sandwichObject.getJSONArray("ingredients");
+
+				sandwich.setIngredients(parseJsonArray(jsonArray));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+			Log.w(TAG, String.format("Unable to parse JSON object. reason: %s", e.getMessage()));
+
+			return null;
+		}
+
+		return sandwich;
+	}
+
+	/**
+	 * Parses an array of strings into a list of strings
+	 * @param array The JSON Array of strings
+	 * @return The list of strings from the array
+	 */
+	private static List<String> parseJsonArray(JSONArray array) {
+		List<String> list = new ArrayList<>();
+
+		try {
+			for (int i = 0; i < array.length(); i++) {
+				final String element = array.getString(i);
+
+				list.add(element);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+			Log.w(TAG, String.format("Unable to parse array. reason: %s", e.getMessage()));
+
+			return Collections.emptyList();
+		}
+
+		return list;
+	}
+
+	/**
+	 * Parses JSON sub-object "name" into the instance's fields
+	 * @param instance The instance where the values are going to be set
+	 * @param object The JSON object where the values are going to get extracted from
+	 * @throws JSONException If one of the expected values does not exist in the sub-object
+	 */
+	private static void parseSandwichName(Sandwich instance, JSONObject object) throws JSONException {
+		if (object != null) {
+			if (object.has("mainName")) {
+				instance.setMainName(object.getString("mainName"));
+			}
+
+			if (object.has("alsoKnownAs")) {
+				final List<String> array = parseJsonArray(object.getJSONArray("alsoKnownAs"));
+
+				instance.setAlsoKnownAs(array);
+			}
+		}
+	}
 }
